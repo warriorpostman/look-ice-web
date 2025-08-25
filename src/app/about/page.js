9a811383a3ba052d605a025220f4dab1c04283e4
Rustam Kasad <rustamkasad@gmail.com>
2025-08-25 16:58:19 -0700

    Added new pages and wired up basic detainers table

---
+++ src/app/about/page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>
+                This app is purpose-built for exploring deportation data 
+                that was received via FOIA requests by the DeportationData 
+                project.
+            </p>
+        </div>
+    );
+};
+
+export default About;
