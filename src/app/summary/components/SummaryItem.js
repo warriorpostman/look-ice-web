71c0bbccf9bfa4f3222fd149eee2ce4f56da8519
Rustam Kasad <rustamkasad@gmail.com>
2025-08-26 10:55:31 -0700

    implemented summaries with charts

---
+++ src/app/summary/components/SummaryItem.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+import './SummaryItem.css';
+
+const SummaryItem = ({ title, labels, values }) => {
+    const data = {
+        labels: labels, 
+        datasets: [
+            {
+            data: values,
+            backgroundColor: ['#0088FE', '#00C49F', '#FFBB28'],
+            },
+        ],
+    };
+
+    return (
+        <div>
+
+        <h3>{title}</h3>
+        <div className="summary-item">
+            <div className="summary-table">
+                <table>
+                    <thead><th>STATE</th><th>Count</th></thead>
+                    <tbody>
+                        {labels.map((label, index) => (
+                            <tr key={label}>
+                                <td>{label}</td>
+                                <td>{values[index]}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            
+            </div>
+            <div className="summary-chart">
+                <Pie 
+                    data={data} 
+                    width={"200px"}
+                    height={"200px"}
+                />
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default SummaryItem;
