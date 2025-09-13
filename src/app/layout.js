79a25694504fdcf6187e30706f9c15baa66205c0
Rustam Kasad <rustamkasad@gmail.com>
2025-09-13 08:01:29 -0700

    add logo, fix margins and layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import Menu from "./components/Menu";
 import ProjectTask from "./components/ProjectTask";
 const isDevelopment = process.env.NEXT_PUBLIC_IS_DEVELOPMENT === 'true';
@@ -24,7 +23,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <h1>lookice</h1> 
+        <div className="site-header">
+          <div>
+            <img src="/logoipsum-401.svg" alt="logo" />
+          </div>
+          <div>
+            <h1>lookice</h1> 
+          </div>
+        </div>
         <main>
           <Menu />
           {children}
@@ -40,27 +46,9 @@ export default function RootLayout({ children }) {
                     status="TODO" 
                 />
                 <ProjectTask 
-                    title="Desktop layout is too wide" 
-                    description="Add some margin to the desktop layout so it's not too wide"
-                    urgency="HIGH"
-                    status="TODO" 
-                />
-                <ProjectTask 
                     title="Remove duplicate arrests" 
                     description="DeportationData site says there's duplicate arrets. See what's needed to remove them"
                     urgency="HIGH"
-                    status="TODO" 
-                />
-                <ProjectTask 
-                    title="Parse criminality statuses to be user friendly" 
-                    description="Parse criminality statuses to be user friendly with function"
-                    urgency="HIGH"
-                    status="TODO" 
-                />
-                <ProjectTask 
-                    title="Add dummy logo to header" 
-                    description="Look at logoipsum for dummy logo"
-                    urgency="MEDIUM"
                     status="TODO" 
                 />
                 <ProjectTask 
